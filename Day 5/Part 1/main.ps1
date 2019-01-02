3f8f53d357bf8074c4aed9690ce64a9eef7c27a2
Jacopo Galati <jacopo.galati@outlook.com>
2019-01-02 10:28:25 +0100

    added day 5 part 1

---
+++ Day 5/Part 1/main.ps1
@@ -0,0 +1,16 @@
+$content = Get-Content -Path ($PSScriptRoot + "\input.txt")
+
+do {
+    $current = $content    
+    
+    foreach ($i in 0..25) {
+        $lower = [string][char]([int]'a'[-0] + $i )
+        $upper = $lower.ToUpper()
+        
+        $content = $content.Replace($lower + $upper, "").Replace($upper + $lower, "")
+
+    }
+    
+} until ($current -eq $content)
+
+$content.Length
