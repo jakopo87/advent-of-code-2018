d66eba3b2546ec4853f59a853e68e5e38e9ecca9
Jacopo Galati <jacopo.galati@outlook.com>
2019-01-02 10:41:13 +0100

    added day 5 part 2

---
+++ Day 5/Part 2/main.ps1
@@ -0,0 +1,24 @@
+$content = Get-Content -Path ($PSScriptRoot + "\input.txt")
+$min = $content.Length
+
+foreach ($i in 0..25) {
+    $lower = [string][char]([int]'a'[-0] + $i )
+    $polymer = $content.Replace($lower, "").Replace($lower.ToUpper(), "")
+
+    do {
+        $current = $polymer    
+        
+        foreach ($i in 0..25) {
+            $lower = [string][char]([int]'a'[-0] + $i )
+            $upper = $lower.ToUpper()
+            
+            $polymer = $polymer.Replace($lower + $upper, "").Replace($upper + $lower, "")
+            
+        }
+        
+    } until ($current -eq $polymer)
+
+    $min = [math]::Min($min, $polymer.Length)
+}
+
+$min
